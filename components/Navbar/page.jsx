2088eb37499dcdb14da4ad6366e4b8836dbe33f8
Samar Abbas <samarabbas172003@gmail.com>
2025-05-25 03:59:26 +0530

    nav1

--- components/Navbar/page.jsx
+++ components/Navbar/page.jsx
@@ -1,16 +1,18 @@
 import { SiSharex } from "react-icons/si";
-
+import Link from "next/link";
 const Navbar = () => {
   return (
     <div>
-      <div
-        className="h-14 text-white bg-gradient-to-br from-sky-900 via-blue-900 to-indigo-950
-
-"
-      >
+      <div className="h-14 flex items-center justify-between px-5 bg-transparent drop-shadow-2xl shadow-sm shadow-black  ">
         <section className="logo flex gap-2 text-2xl font-bold font-mono p-2 items-center ">
           <SiSharex />
           TubeGate
+        </section>
+        <section>
+          <ul>
+            <Link href="/register">Register</Link>
+            <Link href="/register">Login</Link>
+          </ul>
         </section>
       </div>
     </div>
