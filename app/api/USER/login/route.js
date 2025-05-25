91fd5d134350b57e9dd9ad90ed9942f18f50b50a
Samar Abbas <samarabbas172003@gmail.com>
2025-05-25 05:41:08 +0530

    created api for registeration and login

--- app/api/USER/login/route.js
+++ app/api/USER/login/route.js
@@ -2,17 +2,19 @@ import prisma from "DB/db.config";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const {email,password}=await request.json();
+  const { email, password } = await request.json();
   try {
-    const isUser=await prisma.user.findUnique(email);
-    if(!isUser){
-        return NextResponse.json({message:"user does not exist"});
-        
+    const isUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    if (!isUser) {
+      return NextResponse.json({ message: "user does not exist" });
     }
-
-    
-  } catch (error) {
-    
-  }
-    
+    if (isUser.password != password) {
+      return NextResponse.json({ message: "Incorrect password" });
+    }
+    return NextResponse.json({ message: "Login Successfully", isUser });
+  } catch (error) {}
 }
