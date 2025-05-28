47011986bf1181d6dd71383aacdcdf6578b3745a
Samar Abbas <samarabbas172003@gmail.com>
2025-05-29 00:04:23 +0530

    set access-token

--- app/api/AuthHandler/route.js
+++ app/api/AuthHandler/route.js
@@ -1,6 +1,7 @@
 // app/api/AuthHandler/route.js
 import { google } from "googleapis";
 import { NextResponse } from "next/server";
+import { serialize } from "cookie";
 
 export async function GET(request) {
   const url = new URL(request.url);
@@ -20,7 +21,15 @@ export async function GET(request) {
     const { tokens } = await oauth2Client.getToken(code);
     console.log("Tokens:", tokens);
 
-    return NextResponse.json(tokens); 
+    const cookie = serialize("access-toekn", tokens.access_token, {
+      maxAge: 60 * 60,
+      httpOnly: true,
+      path: "/",
+    });
+    const response = NextResponse.json({ message: "Token set" });
+
+    response.headers.append("Set-Cookie", cookie);
+    return response;
   } catch (error) {
     console.error("OAuth Error:", error);
     return NextResponse.json(
