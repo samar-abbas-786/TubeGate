015fa551f607efce437d8db6889fa74f7dfb43e5
Samar Abbas <samarabbas172003@gmail.com>
2025-06-04 18:15:28 +0530

    solve rror in {id} editor

--- app/Editor-List/[id]/page.jsx
+++ app/Editor-List/[id]/page.jsx
@@ -17,12 +17,12 @@ const Editor = async ({ params }) => {
     <div className="min-h-screen bg-gradient-to-b from-black to-gray-900 flex items-center justify-center px-4 py-10">
       <div className="bg-gray-800 border border-purple-600 rounded-2xl p-8 max-w-md w-full shadow-lg text-center space-y-6">
         <div className="w-24 h-24 mx-auto rounded-full bg-purple-700/30 border-4 border-purple-500 shadow-md flex items-center justify-center text-white text-3xl font-semibold">
-          {user.name?.charAt(0).toUpperCase()}
+          {user?.name?.charAt(0).toUpperCase()}
         </div>
 
-        <h1 className="text-3xl font-bold text-purple-300">{user.name}</h1>
+        <h1 className="text-3xl font-bold text-purple-300">{user?.name}</h1>
 
-        <p className="text-purple-200 text-sm">{user.email}</p>
+        <p className="text-purple-200 text-sm">{user?.email}</p>
         {/* <a
           className="text-purple-200 text-sm"
           href={`mailto:${user.email}?subject=Hello`}
@@ -31,7 +31,7 @@ const Editor = async ({ params }) => {
         </a> */}
 
         <span className="inline-block mt-2 px-4 py-1 bg-purple-600 text-white rounded-full text-sm uppercase tracking-wider">
-          {user.role}
+          {user?.role}
         </span>
       </div>
     </div>
